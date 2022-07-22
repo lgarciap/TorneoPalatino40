41e03d1ca887c3b23f99ada83fdf8865c6591810
lgarciap <lgarcia.uvg@gmail.com>
2022-07-22 14:23:46 -0600

    Este es mi primer commit

---
+++ Equipo.java
@@ -0,0 +1,265 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Equipo {
+
+    /**
+     * Default constructor
+     */
+    public Equipo() {
+    }
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private int goles;
+
+    /**
+     * 
+     */
+    private int lugar;
+
+    /**
+     * 
+     */
+    private int tirosEsquina;
+
+    /**
+     * 
+     */
+    private int ganados;
+
+    /**
+     * 
+     */
+    private int perdidos;
+
+    /**
+     * 
+     */
+    private int tirosAgol;
+
+    /**
+     * 
+     */
+    private int amarillas;
+
+    /**
+     * 
+     */
+    private int rojas;
+
+    /**
+     * 
+     */
+    private int faltas;
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param value
+     */
+    public void setNombre(String value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param value
+     */
+    public void setNombre(String value) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Equipo() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param value
+     */
+    public void setNombre(String value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getGoles() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setGoles(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getLugar() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setLugar(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getTirosEsquina() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setTirosEsquina(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getGanados() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setGanados(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getPerdidos() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setPerdidos(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getRojas() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setRojas(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getAmarillas() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setAmarillas(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getTirosAgol() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setTirosAgol(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getFaltas() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param value
+     */
+    public void setFaltas(int value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String toString() {
+        // TODO implement here
+        return "";
+    }
+
+}
---
+++ Torneo.java
@@ -0,0 +1,103 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Torneo {
+
+    /**
+     * Default constructor
+     */
+    public Torneo() {
+    }
+
+    /**
+     * 
+     */
+    private Equipo equipo1;
+
+    /**
+     * 
+     */
+    private Equipo equipo2;
+
+    /**
+     * 
+     */
+    private Equipo equipo3;
+
+    /**
+     * 
+     */
+    private void equipo4. Equipo;
+
+    /**
+     * 
+     */
+    private Equipo equipo5;
+
+    /**
+     * @return
+     */
+    public int totalGoles() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int totalTirosEsquina() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int totalAmarillas() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int totalRojas() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param nombre
+     */
+    public void mostrarEstadisticasEquipo(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * @param i 
+     * @param nombre 
+     * @param goles 
+     * @param lugar 
+     * @param tirosEsq 
+     * @param ganados 
+     * @param perdidos 
+     * @param tirosAgol 
+     * @param amarillas 
+     * @param rojas 
+     * @param faltas
+     */
+    public void llenarEquipo(int i, String nombre, int goles, int lugar, int tirosEsq, int ganados, int perdidos, int tirosAgol, int amarillas, int rojas, int faltas) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Torneo() {
+        // TODO implement here
+    }
+
+}
